fb8fb69fb505d47ae07c45712654d8a5e1c18831
chilung <chilung.cs06g@nctu.edu.tw>
2020-11-07 10:55:51 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
-#BATCH_SIZE = 16
-BATCH_SIZE = 4
+BATCH_SIZE = 16
+#BATCH_SIZE = 4
 PROPOSAL_NUM = 6
 CAT_NUM = 4
 INPUT_SIZE = (448, 448)  # (w, h)
-LR = 0.0001
+LR = 0.001
 WD = 1e-4
 SAVE_FREQ = 1
-resume = './models/collect/090.ckpt'
+resume = ''
 test_model = './models/collect/090.ckpt'
 save_dir = './models/'
-
